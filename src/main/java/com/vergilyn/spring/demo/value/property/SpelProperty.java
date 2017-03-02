9d54d692a886be24bf089dfab01041b395b68d87
VergiLyn <vergilyn@vip.qq.com>
2017-03-02 16:07:40 +0800

    2017-03-02

--- src/main/java/com/vergilyn/spring/demo/value/property/SpelProperty.java
+++ src/main/java/com/vergilyn/spring/demo/value/property/SpelProperty.java
@@ -17,13 +17,21 @@ public class SpelProperty {
 	 */
 	@Value("#{base.baseSong}")
 	private String spelSong;
-	/* // @Value("${ '#{anotherObj.media}' }") //这个不支持。
-	 * 解析：由内往外，${spel.mix} = base.song。然后在spel表达式中，（''）表示定义字符串。
-	 * 所以 #{'base.song'} = base.song
+	/* // @Value("${ '#{base.baseSong}' }") //这个不支持。
+	 * 解析：由内往外，${spel.mix} = baseSong。然后在spel表达式中，('')表示定义字符串。
+	 * 所以 #{'baseSong'} = baseSong
 	 */
 	@Value("#{ '${spel.mix}' }")
 	private String mixSong;
+	@Value("#{base.${spel.mix} }") //组合，特别.后面跟的是对象属性。所以要是class中的属性，而不是properties中的key
+	private String mixSong2;
 	
+	public String getMixSong2() {
+		return mixSong2;
+	}
+	public void setMixSong2(String mixSong2) {
+		this.mixSong2 = mixSong2;
+	}
 	public String getSpelName() {
 		return spelName;
 	}
