799c34819a3bb18ab0125f0cf3a8bec9bd7f28c0
VergiLyn <VergiLyn@vip.qq.com>
2017-03-07 16:58:33 +0800

    拦截器，添加备注说明

--- src/main/java/com/vergilyn/demo/springboot/interceptor/InterceptorController.java
+++ src/main/java/com/vergilyn/demo/springboot/interceptor/InterceptorController.java
@@ -4,16 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InterceptorController {
-
+	/* 返回视图用此写法的好处是：在interceptor中postHandle可以通过ModelAndView获取到参数。
+	 */
 	@GetMapping("/interceptor")
+	public ModelAndView index() {
+		Map<String,Object> param = new HashMap<String, Object>();
+		param.put("name", "vergilyn");
+		param.put("blog", "cnblogs.com/vergilyn");
+		
+		return new ModelAndView("interceptor/interceptor", param);
+	}
+	
+/*	@GetMapping("/interceptor")
 	public String index() {
 		return "interceptor/interceptor";
 	}
+*/
 
 	@GetMapping({"/needInterceptor","/notInterceptor"})
 	@ResponseBody
