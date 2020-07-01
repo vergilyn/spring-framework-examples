0b00c5e4d224093a7f1f043a406739ee8e335539
vergilyn <vergilyn@vip.qq.com>
2020-07-01 10:13:53 +0800

    1. upgrade: spring-boot '2.2.8.RELEASE'. 2. add: `spring-feature-examples` module.

---
+++ spring-feature-examples/README.md
@@ -0,0 +1,4 @@
+# spring-feature-examples
+
+- [spring 注解驱动开发](https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg3MzE1NTIzNA==&action=getalbum&album_id=1370633525925576704&subscene=126&scenenote=https%3A%2F%2Fmp.weixin.qq.com%2Fs%3F__biz%3DMzg3MzE1NTIzNA%3D%3D%26mid%3D2247485168%26idx%3D1%26sn%3D26143c4d7f566174d5afb4ea09e88fd2%26chksm%3Dcee5193df992902b3c50846b02c6689cacbf0c4dba3632f4243b210887795b6c0a2b4dcf4234%26scene%3D126%26sessionid%3D1593562833%26key%3D2cc21493b77c18dee17bffa6b0df9be8f28f351565f90f6c1cb7e6146beb09fb367463c4694ef03ae8f7ef865742a6f8d762f7842191ff3dd569bf7204d444417b04027f9176c615d38fb2ece47860a5%26ascene%3D1%26uin%3DMjA1NzE2NzEwNg%253D%253D%26devicetype%3DWindows%2B10%2Bx64%26version%3D62090523%26lang%3Den%26exportkey%3DAXUDnYjq%252BwiyHrHTEbyguZ4%253D%26pass_ticket%3DjwuOGnwnh4ZMcfll5QjFoLgZS3UD1Sx%252BOClJrrBmKBmVLfHqKArausVkf0ClyG2u%26winzoom%3D1#wechat_redirect)
+- [sunshinelyz/spring-annotation](https://github.com/sunshinelyz/spring-annotation)
---
+++ spring-feature-examples/src/main/java/com/vergilyn/examples/feature/BeanLifecycleComponent.java
@@ -0,0 +1,58 @@
+package com.vergilyn.examples.feature;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author vergilyn
+ * @date 2020-07-01
+ */
+@Component
+public class BeanLifecycleComponent implements InitializingBean, DisposableBean, ApplicationContextAware, BeanPostProcessor {
+    private static final AtomicInteger INDEX = new AtomicInteger(0);
+    private ApplicationContext applicationContext;
+
+    public BeanLifecycleComponent(){
+        print("BeanLifecycleComponent()");
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        print("@PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        print("@PreDestroy");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        print("DisposableBean.destroy()");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        print("InitializingBean.afterPropertiesSet()");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        print("ApplicationContextAware.setApplicationContext(ApplicationContext applicationContext)");
+    }
+
+    public void print(String methodName){
+        System.out.printf("[%02d][BeanLifecycleComponent] >>>> %s \r\n", INDEX.incrementAndGet(), methodName);
+    }
+}
